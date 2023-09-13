d66ff1a43afc11d16d3428856862ad1515c41f7d
shatha-jad <110840047+shatha-jad@users.noreply.github.com>
2023-09-13 15:29:21 +0000

    First Commit

---
+++ Audio/static/script.js
@@ -0,0 +1,19 @@
+// static/script.js
+document.addEventListener('DOMContentLoaded', () => {
+    // Automatically trigger file upload when the page loads
+    autoUploadAndPlay();
+    
+    function autoUploadAndPlay() {
+        fetch('/get_processed_audio')
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    let audio = new Audio(data.audio_url);
+                    audio.play();
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+});
