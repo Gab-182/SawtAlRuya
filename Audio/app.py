fb0c174467d33a4be9ce7d71c1eba283ee45b5cc
shatha-jad <110840047+shatha-jad@users.noreply.github.com>
2023-09-13 16:43:43 +0000

    Fifth Commit

--- Audio/app.py
+++ Audio/app.py
@@ -1,41 +1,56 @@
 
-# from flask import Flask, jsonify, send_from_directory
-# import os
+# from flask import Flask, jsonify
+# from textToSpeech import * 
 
 # app = Flask(__name__)
-# UPLOAD_FOLDER = 'uploads'
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ 
+# def generate_frames():
+#     return "hi this is test image description", "output.jpeg"
 
 # @app.route('/')
 # def index():
-#     return send_from_directory('static', 'index.html')
-
-# @app.route('/get_processed_audio')
-# def get_processed_audio():
-#     # Replace with logic to generate and provide the processed audio file's URL
-#     processed_audio_url = 'path/to/processed/audio.mp3'
-#     return jsonify({"success": True, "audio_url": processed_audio_url})
+#     text, filename = generate_frames()
+#     processed_audio_path = textToSpeech(text, filename)
+#     error_audio = 'error.mp3'
+#     if os.path.exists(processed_audio_path):
+#         return jsonify({"success": True, "audio_url": processed_audio_path})
+#     else:
+#         return jsonify({"success": False, "error": error_audio})
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
-# app.py
+
+import os
 from flask import Flask, jsonify
-from textToSpeech import * 
+from textToSpeech import textToSpeech  # Assuming you have a textToSpeech function
 
 app = Flask(__name__)
 
- 
+# Placeholder function for generating text and an image filename
 def generate_frames():
-    return "hi this is test image description", "output.jpeg"
+    text = "hi this is test image description"
+    filename = "output.jpeg"
+    return text, filename
+
+def generate_audio_description():
+    text, filename = generate_frames()
+    processed_audio_path = textToSpeech(text, filename)
+    return processed_audio_path
 
 @app.route('/')
 def index():
-    text, filename = generate_frames()
-    processed_audio_path = textToSpeech(text, filename)
+    processed_audio_path = generate_audio_description()
+    error_audio = 'error.mp3'
+    
+    # Check if the processed audio file exists
     if os.path.exists(processed_audio_path):
-        return jsonify({"success": True, "audio_url": processed_audio_path})
+        audio_url = processed_audio_path
     else:
-        return jsonify({"success": False, "error": "Audio file does not exist"})
+        audio_url = error_audio
+
+    return jsonify({"success": True, "audio_url": audio_url})
 
 if __name__ == "__main__":
     app.run(debug=True)
+
