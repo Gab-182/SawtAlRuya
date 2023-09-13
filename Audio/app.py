903a2cebf063bf8a3ed1b9d06048e25fe0ad0d1f
Salem-1 <ph.ahmedsalem@gmail.com>
2023-09-13 20:21:48 +0400

    merged the totext file

--- Audio/app.py
+++ Audio/app.py
@@ -1,13 +1,17 @@
 import os
 from flask import Flask, jsonify
+from textToSpeech import * 
 
 app = Flask(__name__)
 
+ 
+def generate_frames():
+    return "hi this is test image description", "output.jpeg"
+
 @app.route('/')
 def index():
-    processed_audio_path = 'path/to/processed/audio.mp3'
-
-    # Check if the audio file exists
+    text, filename = generate_frames()
+    processed_audio_path = textToSpeech(text, filename)
     if os.path.exists(processed_audio_path):
         return jsonify({"success": True, "audio_url": processed_audio_path})
     else:
