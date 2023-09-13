c646cd408527e47f02e55c98c42a2e314bd89861
Salem-1 <ph.ahmedsalem@gmail.com>
2023-09-13 15:37:03 +0400

    added tospeech script

---
+++ texttospeec.py
@@ -0,0 +1,28 @@
+from gtts import gTTS
+import os
+from PIL import Image
+import google.generativeai as palm
+import os
+import sys
+
+
+palm.configure(api_key=os.environ.get('PALM'))
+
+def AIprompt(msg):
+    response = palm.chat(messages=msg)
+    return response.last
+# Text you want to convert to speech
+
+# Create a gTTS object
+while 1:
+    txt = input("salemGPTVoice>")
+    text = AIprompt(txt)
+    tts = gTTS(text)
+    tts.save("output.mp3")
+    os.system("open output.mp3")  # Example for Linux
+
+
+# //python -m venv venv
+# source venv/bin/activate
+# pip install gTTS
+# brew install mpg321
