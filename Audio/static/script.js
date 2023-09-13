c67ba9c49cc8e788e0570a62b3009f17c47ca498
shatha-jad <110840047+shatha-jad@users.noreply.github.com>
2023-09-13 15:43:32 +0000

    Second Commit

--- Audio/static/script.js
+++ Audio/static/script.js
@@ -1,19 +1,30 @@
+// // static/script.js
+// document.addEventListener('DOMContentLoaded', () => {
+//     // Automatically trigger file upload when the page loads
+//     autoUploadAndPlay();
+    
+//     function autoUploadAndPlay() {
+//         fetch('/get_processed_audio')
+//             .then(response => response.json())
+//             .then(data => {
+//                 if (data.success) {
+//                     let audio = new Audio(data.audio_url);
+//                     audio.play();
+//                 }
+//             })
+//             .catch((error) => {
+//                 console.error('Error:', error);
+//             });
+//     }
+// });
+
 // static/script.js
 document.addEventListener('DOMContentLoaded', () => {
-    // Automatically trigger file upload when the page loads
-    autoUploadAndPlay();
+    const audioSource = document.getElementById('audioSource');
     
-    function autoUploadAndPlay() {
-        fetch('/get_processed_audio')
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    let audio = new Audio(data.audio_url);
-                    audio.play();
-                }
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }
+    // Set the src attribute of the audio element to the processed audio URL
+    audioSource.src = processed_audio_url;
+
+    // Play the audio automatically
+    audioSource.play();
 });
